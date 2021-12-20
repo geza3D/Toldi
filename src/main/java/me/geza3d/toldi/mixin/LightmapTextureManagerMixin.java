37071fa1e4e4ecdd0de17c75f363500ee46af089
geza3D <geza3D@gmail.com>
2021-12-20 20:52:15 +0100

    Crash Fix

--- src/main/java/me/geza3d/toldi/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/me/geza3d/toldi/mixin/LightmapTextureManagerMixin.java
@@ -2,18 +2,23 @@ package me.geza3d.toldi.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import me.geza3d.toldi.events.BrightnessCallback;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
 
 @Mixin(LightmapTextureManager.class)
 public class LightmapTextureManagerMixin {
 
-	@ModifyVariable(method = "update", at = @At(value = "LOAD"), name = "m")
-	public float getGamma(float gamma) {
-		if(BrightnessCallback.EVENT.invoker().spoof() == ActionResult.FAIL) return 1000f;
-		return gamma;
+	@Inject(method = "getBrightness", at = @At(value = "HEAD"), cancellable = true)
+	private void onGetBrightness(World world, int lightLevel, CallbackInfoReturnable<Float> info) {
+		if(BrightnessCallback.EVENT.invoker().spoof() == ActionResult.FAIL) {
+			info.setReturnValue(1000f);
+			info.cancel();
+		}
+		
 	}
 }
